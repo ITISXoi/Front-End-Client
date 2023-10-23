3a39832b8e0d8051d7c9139e24c9ea5550e7cce9
CuongHQ <cuongcoca01@gmail.com>
2023-10-24 02:11:16 +0700

    Config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@/styles/css/bootstrap.min.css";
+import "@/styles/css/font-awesome.css";
+import "@/styles/css/ntfs.css";
+import "@/styles/css/responsive.css";
+import "@/styles/css/shortcodes.css";
+import "@/styles/css/style.css";
+import "@/styles/css/textanimation.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
